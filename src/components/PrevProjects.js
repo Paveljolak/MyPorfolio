4721a89eef872c5170568613a0b8aa44fc36e5f6
89201013 <89201013@example.com>
2023-12-02 16:01:59 +0100

    Trying to see if the projects will show up as intended - try2.

--- src/components/PrevProjects.js
+++ src/components/PrevProjects.js
@@ -4,7 +4,7 @@ import { prevProjects } from "../data";
 
 export default function prevProjects() {
   return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+    <section id="prevProjects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
